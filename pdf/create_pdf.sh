cffe035931a32531990744bc2870727a93ded65f
Robb, Sofia <smr@stowers.org>
2017-10-10 14:10:19 -0500

    finally figured out pdfs

--- pdf/create_pdf.sh
+++ pdf/create_pdf.sh
@@ -1,3 +1,8 @@
 ## need to install markdown-pdf 
 ## npm install -g markdown-pdf
-markdown-pdf --css-path css/github-markdown.css ../README.md
+
+#  I don't like the code blocks
+#markdown-pdf --css-path css/github-markdown.css ../README.md
+
+
+pandoc ../README.md --listings -H listing-setup.tex -o pfb2017.lectures.pdf
