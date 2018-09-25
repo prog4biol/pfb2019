2a05a8b70cb844412070d87be8cf7254701476c8
Robb, Sofia <sofia2341@gmail.com>
2018-09-25 13:44:44 -0600

    cleaning out last years files

--- pdfs/old/css/get_css.sh
+++
@@ -1,1 +0,0 @@
-curl -OL https://github.com/sindresorhus/github-markdown-css/blob/gh-pages/github-markdown.css
--- review_sessions/README.md
+++
@@ -1,3 +0,0 @@
-# Review Sessions
-
-This directory is for all notes, slides, and code from review sessions.
