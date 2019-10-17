0c5d2ebbd310b4c6f4ebf8c791279cba187a3f6b
Sofia Robb <sofia2341@gmail.com>
2019-10-17 10:34:38 -0400

    adding count_nts_sets_dict.py

---
+++ scripts/count_nts_sets_dict.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# create a new empty dictionary
+nt_count = {}
+
+# get a set of unique characters in our DNA string
+
+dna = 'GTACCNTTGATTTCGTATTCTGAGAGGCTGCTGCTTAGCGGTAGCCCCTTGGTTTCCGTGGCAACGGAAAA'
+unique = set(dna)
+
+print('unique nt: ', unique) ## {'C', 'A', 'G', 'T', 'N'}
+
+# iterate through each unique nucleotide
+for nt in dna:
+  # count the number of this unique nt in dna
+  count = dna.count(nt)
+
+  # add our count to our dict
+  nt_count[nt] = count
+
+
+print('nt count:', nt_count)
